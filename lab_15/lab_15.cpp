8dcf8d831c6fa5f201f5aed2ce75f5a1dac605bc
Ivan <duzak2006@gmail.com>
2023-12-16 15:04:20 +0200

    Зробив 3 завдання

--- lab_15/lab_15.cpp
+++ lab_15/lab_15.cpp
@@ -1,45 +1,61 @@
 ﻿#include <stdio.h>
+#define MAX_SIZE 100
+
 int main() {
 	int number;
-	printf_s("Press 1 or 2 or 3....or 7");
+	printf_s("Press 1 or 2 or 3....or 7\n");
 	printf_s("1. A program that finds which number is greater between two.\n");
 	printf_s("2. A program that finds the smallest of three numbers.\n");
 	printf_s("3. A program that resets all even elements of the array to zero.\n");
-	printf_s("4. A program that calculates the sum of the elements of an integer array.\n");
-	printf_s("5. A program that displays array elements in reverse order.\n");
-	printf_s("6. A program to store the maximum and minimum values in an array.\n");
-	printf_s("7. The accumulation algorithm is an algorithm used to process data sets with the accumulation of a certain value.\n");
 	scanf_s("%d", &number);
 	switch (number) {
 	case 1: {
 		int x, y;
 		printf_s("Enter value x and y\n");
-		scanf_s("%d %d", &x &y);
+		scanf_s("%d %d", &x, &y);
 		if (x > y) {
-			printf_s("The lagest number\n", x);
+			printf_s("The lagest number is %d\n", x);
 		}
-		else {
-			printf_s("The lagest number\n", y);
+		if (x < y) {
+			printf_s("The lagest number is %d\n", y);
 		}
-		if (x = y) {
-			printf_s("Value are the same\n", x, y);
+		if (x == y) {
+			printf_s("Value are the same\n");
 		}
 	}
 		  break;
 	case 2: {
 		int x, y, z;
 		printf_s("Enter value x, y and z\n");
-		scanf_s("%d %d %d", &x & y & z);
+		scanf_s("%d %d %d", &x, &y, &z);
 		if (x < y && x < z) {
-			printf("Min x");
+			printf("Min x=%d", x);
 		}
 		if (y < x && y < z) {
-			printf("Min y");
+			printf("Min y=%d", y);
 		}
 		if (z < x && z < y) {
-			printf("Min z");
+			printf("Min z=%d", z);
+		}
+		if (x == y && x == z) {
+			printf_s("Value are the same");
 		}
 	}
+		  break;
+	case 3: {
+		int array[MAX_SIZE], n;
+		printf_s("Enter size of massive(not more than %d): ", MAX_SIZE);
+		scanf_s("%d", &n);
+		for (int i = 0; i < n; i++) {
+			if (array[i] % 2 == 0) {
+				array[i] = 0;
+			}
+			else {
+				printf_s("There are no whole numbers");
+			}
+		}
+	}
+		  break;
 	default:
 		printf_s("Wrong number");
 	}
